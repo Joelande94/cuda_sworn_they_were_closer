f2daadc1468b6dd9a755ac8a63b3f2033d360215
Joelande <joelande94@gmail.com>
2019-02-05 22:57:51 +0200

    working version

--- galaxy_program.cu
+++ galaxy_program.cu
@@ -71,17 +71,17 @@
 using namespace std;
 
 
-#define PI 3.14159265359
-#define TO_DEGREES 180.0/PI
+#define PI 3.14159265359f
+#define TO_DEGREES 180.0f/PI
 
-#define BIN_WIDTH 0.25
-#define BIN_MIN 0.0
-#define BIN_MAX 180.0
-#define NUMBER_OF_BINS (int)(BIN_MAX*(1.0/BIN_WIDTH))
+#define BIN_WIDTH 0.25f
+#define BIN_MIN 0.0f
+#define BIN_MAX 180.0f
+#define NUMBER_OF_BINS (int)(BIN_MAX*(1.0f/BIN_WIDTH))
 
 
 float arcmins_to_radians(float minutes){
-    return 1.0/60.0*minutes/180.0;
+    return 1.0f/60.0f*minutes/180.0f;
 }
 
 class GalaxyFile{
@@ -133,52 +133,24 @@ GalaxyFile readFile(string filename)
 
 
 __global__
-void angle_between_galaxies(int i, float *alphas1, float *deltas1, float *alphas2, float *deltas2, int *gpu_hist){
+void angle_between_galaxies(float *alphas1, float *deltas1, float *alphas2, float *deltas2, int *gpu_hist){
     int idx = blockDim.x*blockIdx.x + threadIdx.x;
-    float alpha1 = alphas1[i];
-    float delta1 = deltas1[i];
-    float alpha2 = alphas2[idx];
-    float delta2 = deltas2[idx];
-
-    float angle = 0;
-
-    // Don't do duplicates
-    if( alpha1 != alpha2 && delta1 != delta2 ){
-        // Also try floating point version of sin and cos
-    	float x = sin(delta1) * sin(delta2) + cos(delta1) * cos(delta2) * cos(alpha1 - alpha2);
-        // fminf and fmaxf (x, 1.0f) might need the f on the end.
-        x = fminf(x, 1.0f);
-        x = fmaxf(-1.0f, x);
-        angle = acos(x); // try acosf() or facosf()??
-        angle = angle * TO_DEGREES;
-    }
-        
-    __shared__ int shared_hist[NUMBER_OF_BINS];
-    if(threadIdx.x == 0){
-        for (int i=0; i<NUMBER_OF_BINS; i++) {
-            shared_hist[i] = 0;
-        }
-    }
-
-    int ix = (int)(floor(angle * (1.0f/BIN_WIDTH))) % NUMBER_OF_BINS;
     
-    // Check that we're not going out of bounds.
-    if(ix < 0){
-        ix = 0;
-    }else if(ix >= NUMBER_OF_BINS){
-        ix = NUMBER_OF_BINS-1;
-    }
-    
+    if(idx < 100000){
+        for(int i=0; i<100000; i++){
+                float angle = 0.0f;
+                // Don't do duplicates
+                if( alphas1[i] != alphas2[idx] && deltas1[i] != deltas2[idx] ) {
+                    // Also try floating point version of sin and cos
+                    float x = sinf(deltas1[i]) * sinf(deltas2[idx]) + cosf(deltas1[i]) * cosf(deltas2[idx]) * cosf(alphas1[i] - alphas2[idx]);
+                    // fminf and fmaxf (x, 1.0f) might need the f on the end.
+                    x = fmaxf(-1.0f, fminf(x, 1.0f));
+                    angle = acosf(x) * TO_DEGREES; // try acosf() or facosf()??
+                }
 
-    __syncthreads();
-    atomicAdd(&shared_hist[ix], 1);
-
-    __syncthreads();
-    // Once for every block, copy the contents of shared_hist into gpu_hist
-    if(threadIdx.x == 0){
-        for (int i=0; i<NUMBER_OF_BINS; i++) {
-            gpu_hist[i] = shared_hist[i];
-            //printf("gpu_hist[%d] = %d\n", i, gpu_hist[i]);
+                int ix = (int)(floor(angle * (1.0f/BIN_WIDTH))) % NUMBER_OF_BINS;
+                __syncthreads();
+                atomicAdd(&gpu_hist[ix], 1);
         }
     }
 }
@@ -199,14 +171,16 @@ int main()
 
 
     // Declare and allocate memory for histogram arrays that will be accessible on CPU
-    int *galaxy1_galaxy1_hist;  // DD
-    int *galaxy1_galaxy2_hist;  // DR
-    int *galaxy2_galaxy2_hist;  // RR
-    galaxy1_galaxy1_hist = (int*) malloc(NUMBER_OF_BINS*sizeof(int));
-    galaxy1_galaxy2_hist = (int*) malloc(NUMBER_OF_BINS*sizeof(int));
-    galaxy2_galaxy2_hist = (int*) malloc(NUMBER_OF_BINS*sizeof(int));
-    int *histogram; histogram = (int *) malloc(NUMBER_OF_BINS*sizeof(int));
-    int *total_histogram; total_histogram = (int *) malloc(NUMBER_OF_BINS*sizeof(int));
+    int *DD_hist;  // DD
+    int *DR_hist;  // DR
+    int *RR_hist;  // RR
+    DD_hist = (int*) malloc(NUMBER_OF_BINS*sizeof(int));
+    DR_hist = (int*) malloc(NUMBER_OF_BINS*sizeof(int));
+    RR_hist = (int*) malloc(NUMBER_OF_BINS*sizeof(int));
+    int *histograms;
+    int *total_histogram;
+    histograms = (int *) malloc(NUMBER_OF_BINS*sizeof(int));
+    total_histogram = (int *) malloc(NUMBER_OF_BINS*sizeof(int));
     float *results; results = (float*) malloc(galaxy_array_size);
 
     memset(total_histogram, 0, NUMBER_OF_BINS*sizeof(int));
@@ -216,27 +190,25 @@ int main()
     float *gpu_deltas1;
     float *gpu_alphas2;
     float *gpu_deltas2;
-    float *gpu_results;
-    int *gpu_histogram;
+    int *gpu_histograms;
 
     // Allocate memory on GPU for angle arrays
     cudaMalloc((void**) &gpu_alphas1, galaxy_array_size);
     cudaMalloc((void**) &gpu_deltas1, galaxy_array_size);
     cudaMalloc((void**) &gpu_alphas2, galaxy_array_size);
     cudaMalloc((void**) &gpu_deltas2, galaxy_array_size);
-    cudaMalloc((void**) &gpu_results, galaxy_array_size);
-    cudaMalloc((void**) &gpu_histogram, NUMBER_OF_BINS*sizeof(int));
+    cudaMalloc((void**) &gpu_histograms, NUMBER_OF_BINS*sizeof(int));
 
 	// Copy angles from CPU onto GPU
 	cudaMemcpy(gpu_alphas1, galaxies1.alphas, galaxy_array_size, cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_deltas1, galaxies1.deltas, galaxy_array_size, cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_alphas2, galaxies2.alphas, galaxy_array_size, cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_deltas2, galaxies2.deltas, galaxy_array_size, cudaMemcpyHostToDevice);
-	cudaMemcpy(gpu_histogram, histogram, galaxy_array_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_histograms, histograms, galaxy_array_size, cudaMemcpyHostToDevice);
     
     int warp_size = 32;
-    int threadsInBlock = 8 * warp_size;
-    int blocksInGrid = ceil((galaxies1.number_of_galaxies + threadsInBlock -1) / threadsInBlock);
+    int threadsInBlock = 16 * warp_size;
+    int blocksInGrid = ceil((galaxies1.number_of_galaxies + threadsInBlock-1) / threadsInBlock);
 
 	// Define the grid size (blocks per grid)
     dim3 dimGrid(blocksInGrid);
@@ -244,23 +216,20 @@ int main()
 	// Define block size (threads per block)
 	dim3 dimBlock(threadsInBlock);
 
-    for(int i=0; i<galaxies1.number_of_galaxies; i++){
-        printf("%d%% done\n", (int) (((float)i/galaxies1.number_of_galaxies)*100));
-        // Write histogram full of zeros
-        cudaMemset(gpu_histogram, 0, histogram_size);
+    // Write histogram full of zeros
+    cudaMemset(gpu_histograms, 0, histogram_size);
 
-        // Calculate angles between galaxies1[i] and every galaxy in galaxies2
-        angle_between_galaxies<<<dimGrid, dimBlock>>>(i, gpu_alphas1, gpu_deltas1, gpu_alphas2, gpu_deltas2, gpu_histogram);
-        
-        // Copy result histogram into CPU histogram
-        cudaMemcpy(histogram, gpu_histogram, histogram_size, cudaMemcpyDeviceToHost);
+    // Calculate angles between galaxies1[i] and every galaxy in galaxies2
+    angle_between_galaxies<<<dimGrid, dimBlock>>>(gpu_alphas1, gpu_deltas1, gpu_alphas2, gpu_deltas2, gpu_histograms);
+    
+    // Copy result histogram into CPU histogram
+    cudaMemcpy(histograms, gpu_histograms, histogram_size, cudaMemcpyDeviceToHost);
 
-        // Add values from result histogram to total histogram
-        for(int j=0; j<NUMBER_OF_BINS; j++){
-            //printf("total_histogram[%d] before: %d vs ", j, total_histogram[j]);
-            total_histogram[j] += histogram[j];
-            //printf("after %d\n", total_histogram[j]);
-        }
+    // Add values from result histogram to total histogram
+    for(int j=0; j<NUMBER_OF_BINS; j++){
+        //printf("total_histogram[%d] before: %d vs ", j, total_histogram[j]);
+        total_histogram[j] += histograms[j];
+        //printf("after %d\n", total_histogram[j]);
     }
     
 	// Free all the memory we allocated on the GPU
@@ -268,8 +237,7 @@ int main()
 	cudaFree( gpu_deltas1 );
 	cudaFree( gpu_alphas2 );
 	cudaFree( gpu_deltas2 );
-	cudaFree( gpu_results );
-	cudaFree( gpu_histogram );
+	cudaFree( gpu_histograms );
 
     /*
     for (int i=0; i<4; i++) {
@@ -290,8 +258,8 @@ int main()
     int wraps = 0;
     // Print each bucket bin that has 1 or more galaxy in it.
     for (int i=0; i<NUMBER_OF_BINS; i++) {
-        float bucket_min = (float)i / (1.0/BIN_WIDTH);
-        float bucket_max = (float)i / (1.0/BIN_WIDTH) + BIN_WIDTH;
+        float bucket_min = (float)i / (1.0f/BIN_WIDTH);
+        float bucket_max = (float)i / (1.0f/BIN_WIDTH) + BIN_WIDTH;
         int bucket_value = total_histogram[i];
 
         if(bucket_value > 0){
